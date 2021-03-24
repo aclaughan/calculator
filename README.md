5b0222422ddea58fa2a0d95cd85260c02c5532b2
Alan Claughan <git@claughan.com>
2021-03-24 09:03:29 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# calculator
+As the name suggests will perform calculations on two numbers. After each calculation you can either perform another operation on the previous answer or start a new calculation. Of cause if you've had enough goodness you can always press 'q' to exit :)
---
+++ main.py
@@ -0,0 +1,75 @@
+import logging
+from art import logo
+
+logging.basicConfig(level = logging.DEBUG)
+
+
+def main():
+    print(logo)
+    calculator()
+
+def add(number1, number2):
+  """
+  returns the sum of number1 and number2
+  """
+  return number1 + number2
+
+def subtract(number1, number2):
+  """
+  returns the difference between number1 and number2
+  """
+  return number1 - number2
+
+def multiply(number1, number2):
+  """
+  returns the product of number1 and number2
+  """
+  return number1 * number2
+
+def divide(number1, number2):
+  """
+  returns the ratio between number1 and number2
+  """
+  return number1 / number2
+
+operations = \
+  {
+    '+': add,
+    '-': subtract,
+    '*': multiply,
+    '/': divide
+  }
+
+def next_number(num1):
+  print("Pick an operator ( ", end ='')
+  for operation in operations:
+    print(f"{operation} ", end='')
+  operator = input(")\n  > ")
+
+  num2 = int(input("What is the next number?\n  > "))
+  calculate = operations[operator]
+
+  answer = calculate(num1, num2)
+  print(f"{num1} {operator} {num2} = {answer}")
+  return answer
+
+def calculator():
+  num1 = int(input("What is the first number?\n  > "))
+
+  answer = next_number(num1)
+
+  while True:
+    user = input(
+      "press 'y' to continue calculating\n" \
+      "press 'n' for a new calculation\n" \
+      "or press 'q' to quit,\n  > ")
+
+    if user == 'y':
+      answer = next_number(answer)
+    elif user == 'n':
+      calculator()
+    else:
+      exit()
+
+if __name__ == '__main__':
+    main()
